f93f487a854d6b7a28457a406fd718c4e8c30b95
MH <dico659@gmail.com>
2024-07-24 13:14:47 +0900

    🚀 Fix: some words to 명희

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -53,7 +53,7 @@ export default function Home() {
             </span>
           </h2>
           <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
+            Find in-depth 명희 information about Next.js features and API.
           </p>
         </a>
 
@@ -70,7 +70,7 @@ export default function Home() {
             </span>
           </h2>
           <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
+            Learn about 명희 in an interactive course with&nbsp;quizzes!
           </p>
         </a>
 
@@ -87,7 +87,7 @@ export default function Home() {
             </span>
           </h2>
           <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
+            Explore starter 명희 for Next.js.
           </p>
         </a>
 
@@ -104,7 +104,7 @@ export default function Home() {
             </span>
           </h2>
           <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+            Instantly deploy your 명희 site to a shareable URL with Vercel.
           </p>
         </a>
       </div>
